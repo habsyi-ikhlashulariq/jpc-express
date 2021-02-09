43e27bca34c2e5a6895038db16a04e3d76f9f9c2
donbay28 <61465876+donbay28@users.noreply.github.com>
2021-02-09 19:58:09 +0700

    Create 2021_02_09_122459_create_destination_table.php

---
+++ database/migrations/2021_02_09_122459_create_destination_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMetodePembayaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destination', function (Blueprint $table) {
+            $table->id();
+            $table->string('jenisTransport', 45);
+            $table->string('kotaAsal', 45);
+            $table->string('tujuan', 45);
+            $table->string('tarif', 45);
+            $table->string('estimasiWaktu', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destination');
+    }
+}
